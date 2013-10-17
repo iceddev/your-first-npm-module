0e970b10eb8baf1c72936873cf1d3b4577965ada
Blaine Bublitz <blaine@iceddev.com>
2013-10-17 13:19:58 -0700

    add 2 more questions, also add headers

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 require('./wrap-console');
 
 var question = require('./question');
@@ -6,25 +8,50 @@ var fs = require('fs');
 
 var sequence = require('when/sequence');
 
-function question1(){
-  var whatIsNpm = fs.readFileSync('./instructions/what_is_npm.txt', 'utf-8');
-  console.log(whatIsNpm);
+function repeat(str, times){
+  return new Array(times + 1).join(str);
+}
 
-  return question('{underline}Challenge{/underline}: View commands available for npm.', 'npm help');
+function header(text){
+  var bar = repeat('#', text.length + 4);
+  console.log('{white}' + bar + '{/white}');
+  console.log('{white}#{/white} {bold}' + text + '{/bold} {white}#{/white}');
+  console.log('{white}' + bar + '{/white}');
+}
+
+function instructions(headerText, filename){
+  header(headerText);
+  console.log(fs.readFileSync('./instructions/' + filename + '.txt', 'utf-8'));
+  console.log('{grey}Note: output may be formatted better when using npm commands outside this tutorial{/grey}');
+}
+
+function question1(){
+  instructions('What is npm?', 'what_is_npm');
+
+  return question('{underline}Challenge{/underline}: View commands available for npm', 'npm help');
 }
 
 function question2(){
-  var discoveringModules = fs.readFileSync('./instructions/discovering_modules.txt', 'utf-8');
-  console.log(discoveringModules);
+  instructions('Detailed Help', 'detailed_help');
 
-  return question('{underline}Challenge{/underline}: Search for lodash\'s pluck method on npm.', 'npm search lodash pluck');
+  return question('{underline}Challenge{/underline}: Use `npm help` to view detailed help about search', 'npm help search');
 }
 
 function question3(){
+  instructions('Discovering Modules', 'discovering_modules');
 
+  return question('{underline}Challenge{/underline}: Search for lodash\'s pluck method on npm', 'npm search lodash pluck');
+}
+
+function question4(){
+  instructions('Getting Detailed Info', 'detailed_info');
+
+  return question('{underline}Challenge{/underline}: Get detailed information on lodash.pluck', 'npm view lodash.pluck');
 }
 
 sequence([
   question1,
-  question2
+  question2,
+  question3,
+  question4
 ]);
