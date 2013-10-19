2476e45e8528e0d1acbc519978c5d642feb2e758
Blaine Bublitz <blaine@iceddev.com>
2013-10-18 17:18:03 -0700

    npm init question

---
+++ questions/7.js
@@ -0,0 +1,13 @@
+var instructions = require('../instructions');
+var question = require('../question');
+
+var fs = require('fs');
+var without = require('lodash.without');
+
+module.exports = function(){
+  instructions('Creating Packages', 'creating_packages');
+
+  return question('{underline}Challenge{/underline}: Scaffold a package.json with npm', function(){
+    return fs.existsSync('./package.json');
+  });
+};
